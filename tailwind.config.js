b2ad4c31a7f34c025ebb24701215273e428f3114
roysaucedo <roy.a.saucedo@gmail.com>
2023-02-15 22:36:20 -0800

    move index for vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
@@ -10,3 +10,5 @@ module.exports = {
   },
   plugins: [],
 }
+
+// content: ["./src/**/*.{html,js}"],
